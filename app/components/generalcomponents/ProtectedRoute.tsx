5d618dac0538193173a27d2c03009e47a32013cf
kingmusab-tmt <114432105+kingmusab-tmt@users.noreply.github.com>
2024-07-20 17:39:39 +0100

    next router bug resolve

--- app/components/generalcomponents/ProtectedRoute.tsx
+++ app/components/generalcomponents/ProtectedRoute.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useSession, signIn } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 const ProtectedRoute = ({
