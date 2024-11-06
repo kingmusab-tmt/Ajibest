48c215fbafda6969ea8b3082c42fe82cf0c38a68
kingmusab-tmt <114432105+kingmusab-tmt@users.noreply.github.com>
2024-11-06 19:54:45 +0100

    advance update and fixing of errors

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,8 @@ import Footer from "../app/components/landingpagecomponents/footer";
 import FAQPage from "../app/components/landingpagecomponents/faq";
 import FeaturedProperties from "./components/landingpagecomponents/featuredproperty";
 import PushNotificationManager from "./components/generalcomponents/Subscribe";
+import Services from "./components/landingpagecomponents/services";
+import CustomerStats from "./components/landingpagecomponents/customerstat";
 
 export default function Home() {
   const [isClient, setIsClient] = useState(false);
@@ -20,9 +22,11 @@ export default function Home() {
       <Navbar key="navbar" />
       <Hero key="hero" />
       <Diagram key="diagram" />
+      <Services />
       <FeaturedProperties key="featured-properties" />
       <WhyChooseUs key="why-choose-us" />
       <FAQPage key="faq-page" />
+      <CustomerStats />
       <Footer key="footer" />
     </>
   );
---
+++ app/privacy/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Privacy from "../components/landingpagecomponents/privacy";
+import Navbar from "../components/landingpagecomponents/navbar";
+import Footer from "../components/landingpagecomponents/footer";
+
+const PrivacyPolicy = () => {
+  return (
+    <div>
+      <Navbar />
+      <Privacy />
+      <Footer />
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
