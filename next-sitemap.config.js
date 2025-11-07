d993aed4b5084d0b2132f14951fd70fc8781b617
kingmusab-tmt <114432105+kingmusab-tmt@users.noreply.github.com>
2025-11-07 07:31:06 +0100

    updated sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "ajibest.vercel.app", // replace with your website
+  siteUrl: "ajibestlandvendors.com.ng", // replace with your website
   generateRobotsTxt: true, // optionally generate robots.txt
   changefreq: "daily",
   priority: 0.7,
